a1c08f6b73abcecad057c498219d7e3df7df08a4
jackdoyle5225 <jackdoyle12345@gmail.com>
2022-04-27 16:30:12 +0100

    added getters and setters

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -12,8 +12,46 @@ public class Nematode {
     private char gender;
     private int eyes;
 
-    
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+    
 }
 
 // name,length,limbs,gender,eyes
