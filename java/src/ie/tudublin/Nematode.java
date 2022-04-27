f84f07290e7d93843c35b6cc9d6e8c498e2c84e5
jackdoyle5225 <jackdoyle12345@gmail.com>
2022-04-27 16:21:12 +0100

    added necessary fields

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -6,5 +6,14 @@ import processing.data.TableRow;
 
 public class Nematode {
 
+    private String name;
+    private int length;
+    private int limbs;
+    private char gender;
+    private int eyes;
+
+    
 
 }
+
+// name,length,limbs,gender,eyes
